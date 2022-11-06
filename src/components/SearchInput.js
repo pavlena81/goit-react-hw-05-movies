79413a9f788024546f6ea65dbc012ca44257d169
Olena Nesteruk <pavlena81@gmail.com>
2022-11-06 19:56:50 +0200

    add navigate

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,11 +1,14 @@
 import { Wrapper, Input, Icon } from "./SearchInput.styled";
 
-export const SearchInput = ({ value, onChange }) => {
+
+export const SearchInput = ({ value, onChange}) => {
+  
+
    return (
     <Wrapper>
       <Icon />
       <Input
-        type="text"
+        type="text"        
         value={value}
         onChange={(e) => onChange(e.target.value)}
       />
--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -6,38 +6,54 @@ import { MoviesList } from "components/MoviesList/MoviesList";
 import { SearchInput } from "components/SearchInput";
 import { searchMovie } from "services/api"; 
 
- const Movies = () => {
+import { Navigate } from "react-router-dom";
+//import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+
+
+const Movies = () => {
+    
     const [query, setQuery] = useState('');
     const [movies, setMovies] = useState([]);
     
     const [searchParams, setSearchParams] = useSearchParams();
+    const queryTitle = searchParams.get('query') ?? "";
     
     useEffect(() => {
+       
         if (searchParams.get('query')) {
             async function searchMovies() {
             setMovies((await searchMovie(searchParams.get('query'))).results)
-        }
-        
+            }               
+          
         searchMovies()
         }
         
+        
     },[query, searchParams])
     console.log(movies);
-    const queryTitle = searchParams.get('query') ?? "";
-
     
-    const onSearchInput = e => {
-        // e.preventDefault();
+    
+     
+     
+    const onSearchInput = e => {       
         const value = e;       
-        setQuery(value);
-        setSearchParams({ query: value });
+        setQuery(value);          
+               
+        setSearchParams({ query: value });  
+        
     }
     
+      
     return (
         <main>
             <SearchInput value={queryTitle} onChange={onSearchInput} />
-            <MoviesList movies={movies} />
-           
+            {movies.length === 0 && queryTitle ?
+                (<Navigate to="/movies" replace />)
+                    //, Notify.info('Sorry, there isn`t any movies'))
+                :(<MoviesList movies = { movies } />
+           )}
+            
             
 
         </main>
